c0d8022252e63547c7b1a95622536309a9b80c9d
Lysa-Gomes <laysagomesmello9@gmail.com>
2022-06-20 14:21:13 -0300

    Aprendendo for e while

---
+++ script.js
@@ -0,0 +1,18 @@
+for (let contagem = 1; contagem <=10; contagem++){
+    console.log(contagem)
+}
+
+let contador = 10;
+console.log(contador)
+while(contador >=1){
+    console.log(contador--)
+}
+
+for (let impares = 1; impares <=100; impares+=2){
+    console.log(impares)
+}
+let pares = 0;
+while(pares <=100){
+    console.log(pares)
+    pares+=2
+}
